f16b1552b020c29fe0db1a70bc6023e25195f864
Beta Lin <sb2828sb@gmail.com>
2021-07-10 19:11:51 +0800

    final tune and add README.md

---
+++ README.md
@@ -0,0 +1,12 @@
+# Ping Pong Score Keeper
+<hr>
+
+This is the section 26 exercise in the [The Web Developer Bootcamp 2021](https://www.udemy.com/course/the-web-developer-bootcamp/) (by Colt Steele) 
+
+I wrote the layout in CSS on my own. It's a great exercise to practice the JavaScript DOM and EventListener. By spending extra time on the CSS layout, I got more familiar with CSS and tried more detailed button actions designs.
+
+### ScreenShot
+![image](./screenshot.PNG)
+
+### Try it!
+
--- app.js
+++ app.js
@@ -4,37 +4,38 @@ const player2Btn = document.querySelector("#player2");
 const player2Score = document.querySelector("#player2Score");
 const resetBtn = document.querySelector("#reset");
 const winnerScore = document.querySelector("#winnerScore");
-console.log(winnerScore.value)
-
 
 let score1 = 0;
 let score2 = 0;
+let winningScore = parseInt(winnerScore.value);
 
 player1Btn.addEventListener("click", e => {
     score1 += 1;
     player1Score.textContent = score1;
-    if (score1 == winnerScore.value) {
+    if (score1 === winningScore) {
         // console.log("player 1 win!")
         player1Btn.disabled = true;
         player2Btn.disabled = true;
         player1Score.style.color = "#06d6a0";
         player2Score.style.color = "#ef476f";
     }
-})
+});
 
 player2Btn.addEventListener("click", e => {
     score2 += 1;
     player2Score.textContent = score2;
-    if (score2 == winnerScore.value) {
+    if (score2 === winningScore) {
         // console.log("player 2 win!")
         player1Btn.disabled = true;
         player2Btn.disabled = true;
         player2Score.style.color = "#06d6a0";
         player1Score.style.color = "#ef476f";
     }
-})
+});
 
-resetBtn.addEventListener("click", e => {
+resetBtn.addEventListener("click", reset);
+
+function reset() {
     score1 = 0;
     score2 = 0;
     player1Score.textContent = score1;
@@ -43,5 +44,9 @@ resetBtn.addEventListener("click", e => {
     player2Btn.disabled = false;
     player2Score.style.color = "black";
     player1Score.style.color = "black";
-})
+}
 
+winnerScore.addEventListener("change", e => {
+    winningScore = parseInt(winnerScore.value);
+    reset();
+});
